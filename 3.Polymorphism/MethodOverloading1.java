d2a1ddbb1ce0a144a7e4323e8ec5293bebf05a68
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-04-17 15:22:22 +0530

    Create MethodOverloading1.java

---
+++ 3.Polymorphism/MethodOverloading1.java
@@ -0,0 +1,20 @@
+// Method overloading : -> Method overloading is when two or more methods 
+ // in a class have the same name but different parameters
+ class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int add(int x, int y, int z) {
+        return x + y + z;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(2, 3)); // Output: 5
+        System.out.println(calculator.add(2, 3, 4)); // Output: 9
+    }
+}
