8aa5ca368c6792249a832b93a8884036de4b4457
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-05-14 13:13:52 +0530

    Update MethodOverriding.java

--- 3.Polymorphism/MethodOverriding.java
+++ 3.Polymorphism/MethodOverriding.java
@@ -1,24 +1,34 @@
 // Method Overriding : -> Method overriding is when a subclass provides a specific 
 // implementation of a method that is already provided by its parent class
 
- class Animal {
+public class Animal {
     public void makeSound() {
         System.out.println("The animal makes a sound");
     }
 }
 
- class Cat extends Animal {
+public class Cat extends Animal {
+    @Override
     public void makeSound() {
         System.out.println("Meow");
     }
 }
 
-public class MethodOverriding{
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
+public class MethodOverriding {
     public static void main(String[] args) {
         Animal animal1 = new Animal();
         Animal animal2 = new Cat();
+        Animal animal3 = new Dog();
 
         animal1.makeSound(); // Output: The animal makes a sound
         animal2.makeSound(); // Output: Meow
+        animal3.makeSound(); // Output: Woof
     }
 }
