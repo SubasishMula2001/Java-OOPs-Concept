e8ce704ba973fc977d90677484da681ead2b4942
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-07-30 22:22:45 +0530

    Update Interface2.java

--- 4.Abstraction/Interface2.java
+++ 4.Abstraction/Interface2.java
@@ -30,3 +30,34 @@ public class Interface2 {
         
     }
 }
+// 2nd code
+// Define an interface called "Shape"
+interface Shape {
+    void draw(); // This method will be implemented by classes that implement the Shape interface
+}
+
+// Implement the interface in a class called "Circle"
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+// Implement the interface in a class called "Rectangle"
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(); // Create a Circle object and reference it with the Shape interface
+        Shape rectangle = new Rectangle(); // Create a Rectangle object and reference it with the Shape interface
+        
+        circle.draw(); // This will call the draw() method in the Circle class
+        rectangle.draw(); // This will call the draw() method in the Rectangle class
+    }
+}
