d253c0352461dd7676f2c0821b2cb02a4d1ec305
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-09-19 23:13:18 +0530

    Update Abstraction.java

--- 4.Abstraction/Abstraction.java
+++ 4.Abstraction/Abstraction.java
@@ -52,3 +52,41 @@ public class Loan extends Bank{
         l.pnb();
     }
 }
+// another example
+ abstract class Animal {
+    String color;
+    Animal() {
+        color = "brown";
+    }
+    void eat() {
+      System.out.println("Animal eats");
+  }
+  abstract void walk(); // not obj creation
+}
+class Horse extends Animal {
+    void changeColor() {
+       color="dark brown"; 
+    }
+    void walk() {
+        System.out.println("Walks on 4 legs");
+    }
+}
+class Chicken extends Animal {
+    void changeColor() {
+       color="yellow"; 
+    }
+    void walk() {
+        System.out.println("Walks on 2 legs");
+    }
+}
+public class AbstractClass {
+    public static void main(String args[]) {
+     Horse h = new Horse();
+     h.eat();
+     h.walk();
+     System.out.println(h.color);
+     Chicken c = new Chicken();
+     c.eat();
+     c.walk();
+    }
+}
