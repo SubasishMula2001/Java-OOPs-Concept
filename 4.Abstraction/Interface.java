7b9b104ae5510f598ef11fb2fc51a436f45cd466
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-09-19 23:05:39 +0530

    Update Interface.java

--- 4.Abstraction/Interface.java
+++ 4.Abstraction/Interface.java
@@ -1,18 +1,17 @@
-interface Animal { // by default public static final hota hai
-    
-  void walk(); // abstract and public 
+interface Bank { // by default public static final hota hai
+    void sbi(); // abstract and public 
+    void pnb();
 }
-interface Herbivore {
-    
-}
-class Horse implements Animal, Herbivore {
-    public void walk () {
-        System.out.println("walks on 4 legs");
+public class Loan implements Bank{
+    public void sbi() {
+        System.out.println("SBI Loan");
+    }
+    public void pnb() {
+        System.out.println("PNB Loan");
+    }
+    public static void main(String []args) {
+        Loan l = new Loan();
+        l.sbi();
+        l.pnb();
     }
 }
-public class Interface {
-    public static void main(String []args) {
-        Horse horse = new Horse();
-        horse.walk();
-      }
-   }
