24146cd37eb04750d3df312c04dde71b5376898f
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-09-19 23:15:13 +0530

    Update Abstraction.java

--- 4.Abstraction/Abstraction.java
+++ 4.Abstraction/Abstraction.java
@@ -90,3 +90,29 @@ public class AbstractClass {
      c.walk();
     }
 }
+// another example
+public abstract class Bank {
+   String name;
+   
+   abstract void readData();
+   abstract void writeData();
+}
+class Admin extends Bank {
+    void readData() {
+        System.out.print("This is read data by abstract")
+    }
+    void writeData() {
+        System.out.print("This is write data by abstract ")
+    }
+}
+class Marketing extends Bank {
+    @Override
+    void readData() {
+        
+    }
+    @Override
+    void writeData() {
+        
+    }
+}
+
