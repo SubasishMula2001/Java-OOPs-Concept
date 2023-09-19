cefbd63c9d00e7b8c3b7e9119318edfb24d2ba47
Subasish Mula <74977100+SubasishMula2001@users.noreply.github.com>
2023-09-19 23:16:28 +0530

    Update MethodOverloading.java

--- 3.Polymorphism/MethodOverloading.java
+++ 3.Polymorphism/MethodOverloading.java
@@ -1,10 +1,12 @@
 // Polymorphism means "many forms" and it refers to the ability of an object to take
 // on many forms or to have multiple behaviors.
+
 // Method overloading : -> Method overloading is when two or more methods 
 // Compile-time polymorphism
  // in a class have the same name but different parameters
  // This is the ability of an object to take on many forms.
   // In Java, polymorphism is achieved by using method overloading and method overriding
+
  class Calculator {
     public int add(int x, int y) {
         return x + y;
@@ -21,5 +23,31 @@ public class MethodOverloading {
 
         System.out.println(calculator.add(2, 3)); // Output: 5
         System.out.println(calculator.add(2, 3, 4)); // Output: 9
+    }
+}
+// anotehr example
+
+ class Student {
+     String name;
+     int age;
+     
+  public void printInfo(String name) {
+    System.out.println(name);
+}
+  public void printInfo(int age) {
+    System.out.println(age);
+}
+  public void printInfo(String name, int age) {
+    System.out.println(name +" "+ age);
+  }
+}
+
+public class MethodOverloading2 {
+    public static void main(String args[]) {
+    Student s1 = new Student();
+    s1.name = "subasish";
+    s1.age = 25;
+
+     s1.printInfo(s1.name, s1.age);
     }
 }
